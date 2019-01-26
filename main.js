b6c1333c94e9cfc368fa3c5106c6a325fb225f01
Hoang Anh Tuan <hoangtuan1501@gmail.com>
2019-01-26 15:53:09 +0700

    Init project

---
+++ main.js
@@ -0,0 +1,40 @@
+function show_hidden_menu() {
+    var ele = document.getElementById('hidden_menu');
+    if (ele.className.indexOf('w3-show') == -1) {
+        ele.className += ' w3-show w3-top';
+    } else {
+        ele.className = ele.className.replace(' w3-show w3-top', '');
+    }
+}
+
+function set_element_display(elem_id, prop) {
+    document.getElementById(elem_id).style.display = prop;
+}
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
+}
